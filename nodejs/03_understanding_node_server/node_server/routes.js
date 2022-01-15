4bf80cf4c1a7f8557e94156b0e5f9fc15bc52dbe
Marcio Luis Hamasaki da silva <marcio_luis@live.com>
2022-01-15 18:25:46 -0300

    [NODEJS][03_SERVER] Added assignment try-out.

--- nodejs/03_understanding_node_server/node_server/routes.js
+++ nodejs/03_understanding_node_server/node_server/routes.js
@@ -4,34 +4,40 @@ const { request } = require('http');
 const requestHandler = (req, res) => {
     if (req.url === '/') {
         res.write('<html>');
-        res.write('<head><title>My first page!</title></head>');
+        res.write('<head><title>Add user</title></head>');
         res.write(
-            '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+            '<body><form action="/create-user" method="POST"><input type="text" name="user"><button type="submit">Send</button></form></body>'
         );
         res.write('</html>');
         return res.end();
     }
-    if (req.url === '/message' && req.method === 'POST') {
+    if (req.url === '/users') {
+        res.write('<html>');
+        res.write('<head><title>Users page</title></head>');
+        res.write('<body><ul><li>user 1</li><li>user 2</li></ul></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (req.url === '/create-user' && req.method === 'POST') {
         const body = [];
         req.on('data', (chunk) => {
             body.push(chunk);
         });
         return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
+            console.log(`$$$$ added user: ${parsedBody.split('=')[1]}`);
+
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            res.end();
         });
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
-    res.write('<head><title>My first page!</title></head>');
-    res.write('<body><h1>Hello from my Node.JS Server!</h1></body>');
+    res.write('<head><title>Assessment page</title></head>');
+    res.write('<body><h1>Welcome to my assessment try</h1></body>');
     res.write('</html>');
     res.end();
 };
 
-module.exports = requestHandler
+module.exports = requestHandler;
